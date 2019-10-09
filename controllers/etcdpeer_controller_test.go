6a3259acf1f3c56b1818c98bce423cbc2d7b7d8d
James Laverack <james.laverack@jetstack.io>
2019-10-09 07:50:10 +0100

    boop

--- controllers/etcdpeer_controller_test.go
+++ controllers/etcdpeer_controller_test.go
@@ -1,14 +1,13 @@
 package controllers
 
 import (
-	"context"
-	"strings"
+	//	"strings"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
+	//	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
