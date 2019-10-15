5a857b909ff3397473b7f5c94c2e9e5323d169eb
James Laverack <james.laverack@jetstack.io>
2019-10-15 16:21:37 +0100

    Change test to assert on headless service creation
    For now comment out asserting on peers

--- controllers/etcdcluster_controller_test.go
+++ controllers/etcdcluster_controller_test.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	v1 "k8s.io/api/core/v1"
 	"testing"
 	"time"
 
@@ -32,14 +33,57 @@ func (s *controllerSuite) testClusterController(t *testing.T) {
 		err := s.k8sClient.Create(s.ctx, etcdCluster)
 		require.NoError(t, err, "failed to create EtcdCluster resource")
 
-		peers := &etcdv1alpha1.EtcdPeerList{}
-
+		// Assert on headless service
+		service := &v1.Service{}
 		err = try.Eventually(func() error {
-			return s.k8sClient.List(s.ctx, peers, &client.ListOptions{
+			return s.k8sClient.Get(s.ctx, client.ObjectKey{
 				Namespace: namespace,
-			})
+				// Service will have the same name as the etcd cluster we asked to be created
+				Name: etcdCluster.Name,
+			},
+				service)
 		}, time.Second*5, time.Millisecond*500)
-		require.NoError(t, err)
-		require.Lenf(t, peers.Items, 3, "wrong number of peers: %#v", peers)
+		require.NoError(t, err, "failed to find service for EtcdCluster")
+
+		require.Equal(t, v1.ServiceTypeClusterIP, service.Spec.Type, "service was not a ClusterIP service")
+		require.Equal(t, v1.ClusterIPNone, service.Spec.ClusterIP, "service was not a headless service")
+		require.True(t, service.Spec.PublishNotReadyAddresses, "service did not publish not-ready addresses")
+
+		// Assume single owner reference
+		require.Len(t, service.OwnerReferences, 1, "Incorrect number of owners")
+		ownerRef := service.OwnerReferences[0]
+		require.True(t, *ownerRef.Controller, "Service did not have a controller owner reference")
+		require.Equal(t, "etcd.improbable.io/v1alpha1", ownerRef.APIVersion)
+		require.Equal(t, "EtcdCluster", ownerRef.Kind)
+		require.Equal(t, etcdCluster.Name, ownerRef.Name)
+
+		// Selector
+		selector := service.Spec.Selector
+		require.Equal(t, "etcd", selector["app.kubernetes.io/name"])
+		require.Equal(t, etcdCluster.Name, selector["etcd.improbable.io/cluster-name"])
+
+		// Ports
+		ports := service.Spec.Ports
+		require.Contains(t, ports, v1.ServicePort{
+			Name:     "etcd-client",
+			Protocol: "TCP",
+			Port:     2379,
+		})
+		require.Contains(t, ports, v1.ServicePort{
+			Name:     "etcd-peer",
+			Protocol: "TCP",
+			Port:     2380,
+		})
+
+		// Assert on peers
+		//peers := &etcdv1alpha1.EtcdPeerList{}
+		//
+		//err = try.Eventually(func() error {
+		//	return s.k8sClient.List(s.ctx, peers, &client.ListOptions{
+		//		Namespace: namespace,
+		//	})
+		//}, time.Second*5, time.Millisecond*500)
+		//require.NoError(t, err)
+		//require.Lenf(t, peers.Items, 3, "wrong number of peers: %#v", peers)
 	})
 }
