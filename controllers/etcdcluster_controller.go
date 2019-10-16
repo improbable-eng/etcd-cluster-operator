fbb7a1b98938553dc647edfe646ddd75306d734a
Richard Wall <richard.wall@jetstack.io>
2019-10-16 11:28:08 +0100

    Check for existing service

--- controllers/etcdcluster_controller.go
+++ controllers/etcdcluster_controller.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-logr/logr"
 	v1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -19,28 +20,13 @@ type EtcdClusterReconciler struct {
 	Log logr.Logger
 }
 
-// +kubebuilder:rbac:groups=etcd.improbable.io,resources=etcdclusters,verbs=get;list;watch
-// +kubebuilder:rbac:groups=etcd.improbable.io,resources=etcdclusters/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create
-
-func (r *EtcdClusterReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	log := r.Log.WithValues("etcdcluster", req.NamespacedName)
-
-	var cluster etcdv1alpha1.EtcdCluster
-	if err := r.Get(ctx, req.NamespacedName, &cluster); err != nil {
-		log.Error(err, "unable to fetch EtcdCluster")
-		return ctrl.Result{}, client.IgnoreNotFound(err)
-	}
-
-	service := &v1.Service{
+func headlessServiceForCluster(cluster *etcdv1alpha1.EtcdCluster) *v1.Service {
+	return &v1.Service{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      cluster.Name,
 			Namespace: cluster.Namespace,
 			OwnerReferences: []metav1.OwnerReference{
-				*metav1.NewControllerRef(&cluster, etcdv1alpha1.GroupVersion.WithKind("EtcdCluster")),
+				*metav1.NewControllerRef(cluster, etcdv1alpha1.GroupVersion.WithKind("EtcdCluster")),
 			},
 			Labels: map[string]string{
 				appLabel:     appName,
@@ -68,11 +54,37 @@ func (r *EtcdClusterReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error)
 			},
 		},
 	}
-	if err := r.Create(ctx, service); err != nil {
-		log.Error(err, "unable to create Service", "namespace", service.Namespace, "cluster", cluster.Name)
-		return ctrl.Result{}, err
+}
+
+// +kubebuilder:rbac:groups=etcd.improbable.io,resources=etcdclusters,verbs=get;list;watch
+// +kubebuilder:rbac:groups=etcd.improbable.io,resources=etcdclusters/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create
+
+func (r *EtcdClusterReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	log := r.Log.WithValues("etcdcluster", req.NamespacedName)
+
+	cluster := &etcdv1alpha1.EtcdCluster{}
+	if err := r.Get(ctx, req.NamespacedName, cluster); err != nil {
+		log.Error(err, "unable to fetch EtcdCluster")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	service := &v1.Service{}
+	if err := r.Get(ctx, req.NamespacedName, service); err != nil {
+		if !apierrors.IsNotFound(err) {
+			log.Error(err, "unable to fetch EtcdCluster service")
+			return ctrl.Result{}, err
+		}
+		service = headlessServiceForCluster(cluster)
+		if err := r.Create(ctx, service); err != nil {
+			log.Error(err, "unable to create Service", "namespace", service.Namespace, "cluster", cluster.Name)
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
+	}
 	return ctrl.Result{}, nil
 }
 
