bc051d62d9400e5372599e545a8577a289d276bb
Adam Hosier <adamhosier@improbable.io>
2019-10-16 13:39:21 +0100

    e2e

---
+++ internal/test/e2e/kubectl.go
@@ -0,0 +1,34 @@
+package e2e
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+// kubectlContext wraps shell calls to `kubectl', appropriately handling their results. It is only intended for use
+// in tests.
+type kubectlContext struct {
+	t          *testing.T
+	configPath string
+}
+
+func (k *kubectlContext) do(args ...string) ([]byte, error) {
+	k.t.Log("Running kubectl " + strings.Join(args, " "))
+	cmd := exec.Command("kubectl", args...)
+	cmd.Env = append(cmd.Env, "KUBECONFIG="+k.configPath)
+	return cmd.CombinedOutput()
+}
+
+// Apply wraps `kubectl apply', returning any error that occurred.
+func (k *kubectlContext) Apply(args ...string) error {
+	out, err := k.do(append([]string{"apply"}, args...)...)
+	k.t.Log(string(out))
+	return err
+}
+
+// Get wraps `kubectl get', returning the unparsed output & any error that occurred.
+func (k *kubectlContext) Get(args ...string) (string, error) {
+	out, err := k.do(append([]string{"get"}, args...)...)
+	return string(out), err
+}
