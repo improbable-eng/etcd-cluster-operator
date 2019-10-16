130c748688a22227ce2023f5734b644d686473a9
James Laverack <james.laverack@jetstack.io>
2019-10-16 16:57:18 +0100

    Improve structure of testing
    This produces output like:
    ```
    === RUN   TestAPIs
    --- PASS: TestAPIs (5.38s)
    === RUN   TestAPIs/ClusterControllers
        --- PASS: TestAPIs/ClusterControllers (1.04s)
    === RUN   TestAPIs/ClusterControllers/OnCreation
            --- PASS: TestAPIs/ClusterControllers/OnCreation (1.04s)
    === RUN   TestAPIs/ClusterControllers/OnCreation/CreatesService
                --- PASS: TestAPIs/ClusterControllers/OnCreation/CreatesService (0.50s)
    === RUN   TestAPIs/ClusterControllers/OnCreation/CreatesPeers
                --- PASS: TestAPIs/ClusterControllers/OnCreation/CreatesPeers (0.50s)
    ```

--- controllers/etcdcluster_controller_test.go
+++ controllers/etcdcluster_controller_test.go
@@ -19,7 +19,7 @@ import (
 )
 
 func (s *controllerSuite) testClusterController(t *testing.T) {
-	t.Run("TestClusterController_OnCreation_CreatesService", func(t *testing.T) {
+	t.Run("OnCreation", func(t *testing.T) {
 		teardownFunc, namespace := s.setupTest(t)
 		defer teardownFunc()
 
@@ -38,78 +38,81 @@ func (s *controllerSuite) testClusterController(t *testing.T) {
 		err := s.k8sClient.Create(s.ctx, etcdCluster)
 		require.NoError(t, err, "failed to create EtcdCluster resource")
 
-		// Assert on headless service
-		service := &v1.Service{}
-		err = try.Eventually(func() error {
-			return s.k8sClient.Get(s.ctx, client.ObjectKey{
-				Namespace: namespace,
-				// Service will have the same name as the etcd cluster we asked to be created
-				Name: etcdCluster.Name,
-			},
-				service)
-		}, time.Second*5, time.Millisecond*500)
-		require.NoError(t, err, "failed to find service for EtcdCluster")
+		t.Run("CreatesService", func(t *testing.T) {
+			service := &v1.Service{}
+			err = try.Eventually(func() error {
+				return s.k8sClient.Get(s.ctx, client.ObjectKey{
+					Namespace: namespace,
+					// Service will have the same name as the etcd cluster we asked to be created
+					Name: etcdCluster.Name,
+				},
+					service)
+			}, time.Second*5, time.Millisecond*500)
+			require.NoError(t, err, "failed to find service for EtcdCluster")
 
-		require.Equal(t, v1.ServiceTypeClusterIP, service.Spec.Type, "service was not a ClusterIP service")
-		require.Equal(t, v1.ClusterIPNone, service.Spec.ClusterIP, "service was not a headless service")
-		require.True(t, service.Spec.PublishNotReadyAddresses, "service did not publish not-ready addresses")
+			require.Equal(t, v1.ServiceTypeClusterIP, service.Spec.Type, "service was not a ClusterIP service")
+			require.Equal(t, v1.ClusterIPNone, service.Spec.ClusterIP, "service was not a headless service")
+			require.True(t, service.Spec.PublishNotReadyAddresses, "service did not publish not-ready addresses")
 
-		// Check the service's labels
-		require.Equal(t, "etcd", service.Labels["app.kubernetes.io/name"], "Service did not have app name label")
-		require.Equal(t, etcdCluster.Name, service.Labels["etcd.improbable.io/cluster-name"], "Service did not have etcd cluster name label")
+			// Check the service's labels
+			require.Equal(t, "etcd", service.Labels["app.kubernetes.io/name"], "Service did not have app name label")
+			require.Equal(t, etcdCluster.Name, service.Labels["etcd.improbable.io/cluster-name"], "Service did not have etcd cluster name label")
 
-		// Assume single owner reference
-		assertOwnedByCluster(t, etcdCluster, service)
+			// Assume single owner reference
+			assertOwnedByCluster(t, etcdCluster, service)
 
-		// Selector
-		selector := service.Spec.Selector
-		require.Equal(t, "etcd", selector["app.kubernetes.io/name"], "Selector did not select for 'etcd' app name label")
-		require.Equal(t, etcdCluster.Name, selector["etcd.improbable.io/cluster-name"], "Selector did not select for etcd cluster name label")
+			// Selector
+			selector := service.Spec.Selector
+			require.Equal(t, "etcd", selector["app.kubernetes.io/name"], "Selector did not select for 'etcd' app name label")
+			require.Equal(t, etcdCluster.Name, selector["etcd.improbable.io/cluster-name"], "Selector did not select for etcd cluster name label")
 
-		// Ports
-		ports := service.Spec.Ports
-		require.Contains(t, ports, v1.ServicePort{
-			Name:       "etcd-client",
-			Protocol:   "TCP",
-			Port:       2379,
-			TargetPort: intstr.FromInt(2379),
-		}, "Service did not declare client port")
-		require.Contains(t, ports, v1.ServicePort{
-			Name:       "etcd-peer",
-			Protocol:   "TCP",
-			Port:       2380,
-			TargetPort: intstr.FromInt(2380),
-		}, "Service did not declare peer port")
+			// Ports
+			ports := service.Spec.Ports
+			require.Contains(t, ports, v1.ServicePort{
+				Name:       "etcd-client",
+				Protocol:   "TCP",
+				Port:       2379,
+				TargetPort: intstr.FromInt(2379),
+			}, "Service did not declare client port")
+			require.Contains(t, ports, v1.ServicePort{
+				Name:       "etcd-peer",
+				Protocol:   "TCP",
+				Port:       2380,
+				TargetPort: intstr.FromInt(2380),
+			}, "Service did not declare peer port")
+		})
 
-		// Assert on peers
-		peers := &etcdv1alpha1.EtcdPeerList{}
-		err = try.Eventually(func() error {
-			return s.k8sClient.List(s.ctx, peers, &client.ListOptions{
-				Namespace: namespace,
-			})
-		}, time.Second*5, time.Millisecond*500)
-		require.NoError(t, err)
-		require.Lenf(t, peers.Items, 3, "wrong number of peers: %#v", peers)
+		t.Run("CreatesPeers", func(t *testing.T) {
+			// Assert on peers
+			peers := &etcdv1alpha1.EtcdPeerList{}
+			err = try.Eventually(func() error {
+				return s.k8sClient.List(s.ctx, peers, &client.ListOptions{
+					Namespace: namespace,
+				})
+			}, time.Second*5, time.Millisecond*500)
+			require.NoError(t, err)
+			require.Lenf(t, peers.Items, 3, "wrong number of peers: %#v", peers)
 
-		expectedInitialCluster := make([]etcdv1alpha1.InitialClusterMember, len(peers.Items))
-		for i, peer := range peers.Items {
-			expectedInitialCluster[i] = etcdv1alpha1.InitialClusterMember{
-				Name: peer.Name,
-				Host: fmt.Sprintf("%s.%s.%s.svc",
-					peer.Name,
-					etcdCluster.Name,
-					namespace),
+			expectedInitialCluster := make([]etcdv1alpha1.InitialClusterMember, len(peers.Items))
+			for i, peer := range peers.Items {
+				expectedInitialCluster[i] = etcdv1alpha1.InitialClusterMember{
+					Name: peer.Name,
+					Host: fmt.Sprintf("%s.%s.%s.svc",
+						peer.Name,
+						etcdCluster.Name,
+						namespace),
+				}
 			}
-		}
 
-		for _, peer := range peers.Items {
-			assertPeer(t, etcdCluster, &peer)
+			for _, peer := range peers.Items {
+				assertPeer(t, etcdCluster, &peer)
 
-			assert.Equal(t,
-				expectedInitialCluster,
-				peer.Spec.Bootstrap.Static.InitialCluster,
-				"Peer did not have expected static bootstrap instructions")
-		}
+				assert.Equal(t,
+					expectedInitialCluster,
+					peer.Spec.Bootstrap.Static.InitialCluster,
+					"Peer did not have expected static bootstrap instructions")
+			}
+		})
 	})
 }
 
