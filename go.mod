e22d7a71300a6b5941d503d4ab6cf8499af9806c
adamhosier <hosieradam@gmail.com>
2019-10-21 10:39:58 +0100

    Create E2E test suite (#26)

--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module github.com/improbable-eng/etcd-cluster-operator
 go 1.13
 
 require (
+	github.com/coreos/etcd v3.3.17+incompatible // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/dustinkirkland/golang-petname v0.0.0-20190613200456-11339a705ed2
 	github.com/go-logr/logr v0.1.0
 	github.com/stretchr/testify v1.3.0
+	go.etcd.io/etcd v3.3.17+incompatible
 	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
 	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
 	sigs.k8s.io/controller-runtime v0.2.2
 	sigs.k8s.io/controller-tools v0.2.1 // indirect
+	sigs.k8s.io/kind v0.5.1
 )
---
+++ internal/test/e2e/kubectl.go
@@ -0,0 +1,52 @@
+package e2e
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+// kubectlContext wraps shell calls to `kubectl', appropriately handling their results. It is only intended for use
+// in tests.
+type kubectlContext struct {
+	t          *testing.T
+	configPath string
+	homeDir    string
+}
+
+func (k *kubectlContext) do(args ...string) ([]byte, error) {
+	if k.homeDir == "" {
+		hd, err := os.UserHomeDir()
+		if err != nil {
+			return nil, err
+		}
+		k.homeDir = hd
+	}
+
+	k.t.Log("Running kubectl " + strings.Join(args, " "))
+	cmd := exec.Command("kubectl", args...)
+	cmd.Env = append(cmd.Env, "KUBECONFIG="+k.configPath)
+	cmd.Env = append(cmd.Env, "HOME="+k.homeDir)
+	return cmd.CombinedOutput()
+}
+
+// Apply wraps `kubectl apply', returning any error that occurred.
+func (k *kubectlContext) Apply(args ...string) error {
+	out, err := k.do(append([]string{"apply"}, args...)...)
+	k.t.Log(string(out))
+	return err
+}
+
+// Patch wraps `kubectl patch', returning any error that occurred.
+func (k *kubectlContext) Patch(args ...string) error {
+	out, err := k.do(append([]string{"patch"}, args...)...)
+	k.t.Log(string(out))
+	return err
+}
+
+// Get wraps `kubectl get', returning the unparsed output & any error that occurred.
+func (k *kubectlContext) Get(args ...string) (string, error) {
+	out, err := k.do(append([]string{"get"}, args...)...)
+	return string(out), err
+}
