2d22308708f95bccf16baf361f2c709e366c8c9d
Richard Wall <richard.wall@jetstack.io>
2019-10-22 13:28:49 +0100

    Some more success cases for CheckStructFields

--- internal/test/try/try_test.go
+++ internal/test/try/try_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 )
 
@@ -134,6 +135,10 @@ func TestEventually(t *testing.T) {
 	}
 }
 
+// TODO(wallrj): Add tests for failure cases.
+// For this I searched for a way to modify / wrap `testing.T` so that it fails
+// if the there are no assertion errors, and succeeds if there are errors, with
+// some way to check the assertion message.
 func TestCheckStructFields(t *testing.T) {
 	for _, tc := range []struct {
 		name          string
@@ -142,13 +147,19 @@ func TestCheckStructFields(t *testing.T) {
 		actual        interface{}
 	}{
 		{
-			name:          "int",
+			name:          "int values",
 			path:          "",
 			expectedValue: 123,
 			actual:        123,
 		},
 		{
-			name:          "quantity",
+			name:          "string values",
+			path:          "",
+			expectedValue: "foo",
+			actual:        "foo",
+		},
+		{
+			name:          "quantity values",
 			path:          "",
 			expectedValue: resource.MustParse("123Gi"),
 			actual:        resource.MustParse("123Gi"),
@@ -162,6 +173,37 @@ func TestCheckStructFields(t *testing.T) {
 		//	expectedValue: pointer.StringPtr("foo"),
 		//	actual:        pointer.StringPtr("foox"),
 		// },
+		{
+			name:          "struct paths with maps",
+			path:          ".spec.resources.requests.storage",
+			expectedValue: resource.MustParse("101Gi"),
+			actual: &corev1.PersistentVolumeClaim{
+				Spec: corev1.PersistentVolumeClaimSpec{
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							"storage": resource.MustParse("101Gi"),
+						},
+					},
+				},
+			},
+		},
+		{
+			name:          "struct paths with lists",
+			path:          `.containers[?(@.name=="container2")].image`,
+			expectedValue: "example.com/image2:v1",
+			actual: &corev1.PodSpec{
+				Containers: []corev1.Container{
+					{
+						Name:  "container1",
+						Image: "example.com/image1:v1",
+					},
+					{
+						Name:  "container2",
+						Image: "example.com/image2:v1",
+					},
+				},
+			},
+		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			CheckStructFields(
