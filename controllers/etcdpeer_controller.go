75ab97480b48de56e9b2dca6a17b28d20a6693e5
Richard Wall <richard.wall@jetstack.io>
2019-10-23 16:30:34 +0100

    Validate the EtcdPeer before attempting to reconcile it

--- controllers/etcdpeer_controller.go
+++ controllers/etcdpeer_controller.go
@@ -232,6 +232,12 @@ func (r *EtcdPeerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 
 	log.V(2).Info("Found EtcdPeer", "name", peer.Name)
 
+	// Validate in case a validating webhook has not been deployed
+	err := peer.ValidateCreate()
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
 	// Apply defaults in case a defaulting webhook has not been deployed.
 	peer.Default()
 
