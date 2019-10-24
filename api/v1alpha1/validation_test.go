3ea8de4208e86326f4a7c85a09d9f7a7369f79fb
Richard Wall <richard.wall@jetstack.io>
2019-10-24 14:16:59 +0100

    Use CamelCase test names

--- api/v1alpha1/validation_test.go
+++ api/v1alpha1/validation_test.go
@@ -7,14 +7,14 @@ import (
 )
 
 func TestValidateCreate(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
 		o := ExampleEtcdPeer("ns1")
 		err := o.ValidateCreate()
 		if !assert.NoError(t, err) {
 			t.Log(err)
 		}
 	})
-	t.Run("spec.storage missing", func(t *testing.T) {
+	t.Run("StorageMissing", func(t *testing.T) {
 		o := ExampleEtcdPeer("ns1")
 		o.Spec.Storage = nil
 		err := o.ValidateCreate()
@@ -22,7 +22,7 @@ func TestValidateCreate(t *testing.T) {
 			t.Log(err)
 		}
 	})
-	t.Run("spec.storage.volumeClaimTemplate missing", func(t *testing.T) {
+	t.Run("VolumeClaimTemplateMissing", func(t *testing.T) {
 		o := ExampleEtcdPeer("ns1")
 		o.Spec.Storage.VolumeClaimTemplate = nil
 		err := o.ValidateCreate()
@@ -30,7 +30,7 @@ func TestValidateCreate(t *testing.T) {
 			t.Log(err)
 		}
 	})
-	t.Run("spec.storage.volumeClaimTemplate.storageClassName missing", func(t *testing.T) {
+	t.Run("StorageClassNameMissing", func(t *testing.T) {
 		o := ExampleEtcdPeer("ns1")
 		o.Spec.Storage.VolumeClaimTemplate.StorageClassName = nil
 		err := o.ValidateCreate()
@@ -38,7 +38,7 @@ func TestValidateCreate(t *testing.T) {
 			t.Log(err)
 		}
 	})
-	t.Run("spec.storage.volumeClaimTemplate.resources.storage missing", func(t *testing.T) {
+	t.Run("ResourcesStorageMissing", func(t *testing.T) {
 		o := ExampleEtcdPeer("ns1")
 		delete(o.Spec.Storage.VolumeClaimTemplate.Resources.Requests, "storage")
 		err := o.ValidateCreate()
