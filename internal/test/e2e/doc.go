5eff5a1ea5ba152fedadbe121a7d0a63ca5378f9
Richard Wall <wallrj@users.noreply.github.com>
2020-03-05 16:01:07 +0000

    Update documentation and prevent kind using fancy tty output (#174)

--- internal/test/e2e/doc.go
+++ internal/test/e2e/doc.go
@@ -1,10 +1,4 @@
-// Package e2e holds an end-to-end test framework for testing the operator. The goal is for it to deploy the operator
-// and some custom resources in to a kubernetes cluster, and observe that a reachable ETCD cluster is created.
-// Flags include:
-//   * `--kind': starts a local Kind cluster to run the tests against
-//   * `--current-context': runs the tests against the local kubernetes context. This context must contain a running
-//      operator & have custom resource definitions applied.
-//   * `--repo-root': path to the root of the etcd-cluster-operator repo.
-//   * `--cleanup': defaulting to true, specifies if the Kind cluster should be automatically destroyed when the test
-//      finishes. It can be useful to disable this to debug why the e2e tests are failing.
+// Package e2e holds an end-to-end test framework for testing the operator.
+// These tests are skipped by default and require extra command line arguments.
+// See e2e_test.go
 package e2e
